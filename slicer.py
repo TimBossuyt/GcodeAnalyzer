c3f82637a75dd4ac80be22c77e79bc57a5a88b21
Tim Bossuyt <tim.bossuyt@outlook.com>
2021-03-31 00:15:22 +0200

    getting gcode stats

--- slicer.py
+++ slicer.py
@@ -1,23 +1,28 @@
-# prusa-slicer-console.exe --export-gcode benchy.stl --load Printerlab_0.15mm.ini --load Printerlab_PLA.ini --load Printerlab_Prusa_i3_MK3S.ini
-
 import os
 
 def slice_stl(fileName, layer_height, filament_type, outputFileName):
-    command = "prusa-slicer-console.exe --export-gcode {}.stl --load Printerlab_{}mm.ini --load Printerlab_{}.ini --load Printerlab_Prusa_i3_MK3S.ini --output export-gcodes/{}.gcode".format(fileName, layer_height, filament_type, outputFileName)
+    command = "prusa-slicer-console.exe --export-gcode {}.stl --load Printerlab_{}mm.ini --load Printerlab_{}.ini --load Printerlab_Prusa_i3_MK3S.ini --output export-gcodes/{}.gcode ".format(fileName, layer_height, filament_type, outputFileName)
 
     os.system(command)
 
+slice_stl("benchy", "0.20", "PLA", "benchy123")
+
+def get_stats(fileName):
+    # 8 useful lines of code
+    with open("./export-gcodes/{}.gcode".format(fileName), "r") as f:
+        for num, line in enumerate(f, 1):
+            if "filament used" in line:
+                lineNumber = num
+                break
+        
+        data = f.readlines()[0:7]
 
 
-# slice_stl("benchy", "0.10", "PLA", "benchy123")
+        filament_used = data[0][2:].replace("\n", "").split(" = ")[1]
+        printing_time = data[5][2:].replace("\n", "").split(" = ")[1]
 
-# 8 useful lines of code
-with open("./export-gcodes/benchy123.gcode", "r") as f:
-    for num, line in enumerate(f, 1):
-        if "filament used" in line:
-            lineNumber = num
-            break
+    return {"filament_used": filament_used, "printing_time": printing_time}
 
+print(get_stats("benchy123"))
 
-print(lineNumber)
-
+#to do: setting infill
