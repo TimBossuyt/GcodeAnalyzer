f86755b90b1110c3fd9f54048f0f532196603d1d
Tim Bossuyt <tim.bossuyt@outlook.com>
2021-03-31 12:29:33 +0200

    31/03/2021

--- slicer.py
+++ slicer.py
@@ -1,15 +1,13 @@
 import os
+from datetime import datetime
 
-def slice_stl(fileName, layer_height, filament_type, outputFileName):
-    command = "prusa-slicer-console.exe --export-gcode {}.stl --load Printerlab_{}mm.ini --load Printerlab_{}.ini --load Printerlab_Prusa_i3_MK3S.ini --output export-gcodes/{}.gcode ".format(fileName, layer_height, filament_type, outputFileName)
+def get_stats_from_stl(fileName, layer_height, filament_type, fill_density):
+    output_name = fileName + "_" + datetime.now().strftime("%d_%m_%Y")
+    command = "prusa-slicer-console.exe --export-gcode {}.stl --load print_{}mm.ini --load filament_{}.ini --load Prusa_i3_MK3S.ini --fill-density {} --output export-gcodes/{}.gcode ".format(fileName, layer_height, filament_type, fill_density, output_name)
 
     os.system(command)
 
-slice_stl("benchy", "0.20", "PLA", "benchy123")
-
-def get_stats(fileName):
-    # 8 useful lines of code
-    with open("./export-gcodes/{}.gcode".format(fileName), "r") as f:
+    with open("./export-gcodes/{}.gcode".format(output_name), "r") as f:
         for num, line in enumerate(f, 1):
             if "filament used" in line:
                 lineNumber = num
@@ -17,12 +15,18 @@ def get_stats(fileName):
         
         data = f.readlines()[0:7]
 
-
+        # extracting filament_used and printing_time from list
         filament_used = data[0][2:].replace("\n", "").split(" = ")[1]
         printing_time = data[5][2:].replace("\n", "").split(" = ")[1]
 
-    return {"filament_used": filament_used, "printing_time": printing_time}
+    return {"filament_used (cm3)": filament_used, "printing_time": printing_time}
 
-print(get_stats("benchy123"))
 
-#to do: setting infill
+
+print(get_stats_from_stl("benchy", "0.15", "PLA", "45%"))
+
+
+# Config files:
+#     Printerlab_0.xmm.ini (layer_height, automatic supports on build plate only)
+#     Printerlab_FilamentType.ini (filament_type)
+#     Printerlab_Printer.ini (Printer model)
